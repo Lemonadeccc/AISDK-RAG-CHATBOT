e23cb85488c0567c44399b573f65086f09edeb2b
Lemonadeccc <zwjhb12@163.com>
2026-01-11 14:03:27 +0800

    fix: optimize ui in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,6 @@ export default function Home() {
         <h1 className="text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
           Welcome to RAG Chatbot.
         </h1>
-        <a
-          className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-6 text-base font-medium text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] sm:w-auto"
-          href="/sign-in"
-        >
-          Sign in to chat
-        </a>
       </main>
     </div>
   );
