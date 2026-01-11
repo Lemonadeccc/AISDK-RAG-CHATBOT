3a135e6105fb137e35dc7e96f8a537fd2114187e
Lemonadeccc <zwjhb12@163.com>
2026-01-11 12:30:48 +0800

    feat: add upload pdf

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,13 +1,13 @@
 import { convertToModelMessages, streamText, UIMessage } from "ai";
-// import { openai } from "@ai-sdk/openai";
-import { deepseek } from "@ai-sdk/deepseek";
+import { openai } from "@ai-sdk/openai";
+// import { deepseek } from "@ai-sdk/deepseek";
 
 export async function POST(req: Request) {
   try {
     const { messages }: { messages: UIMessage[] } = await req.json();
     const result = streamText({
-      // model: openai("gpt-5-mini"),
-      model: deepseek("deepseek-chat"),
+      model: openai("gpt-5-mini"),
+      // model: deepseek("deepseek-chat"),
       messages: await convertToModelMessages(messages),
     });
     return result.toUIMessageStreamResponse();
